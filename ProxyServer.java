93217df2a9016e716440755c1a2a14c857bab58d
livelaughconquer <jorgecfelix@gmail.com>
2015-04-06 19:34:18 -0700

    Proxy Actual Working Stuff

---
+++ ProxyServer.java
@@ -0,0 +1,38 @@
+/*
+ *
+ * Created by Jorge Felix and Grant Swenson on 4/5/2015.
+ *
+ * Program Description:
+ Step 2 -  Developing a primitive web server
+ Write a primitive Java web server that would just get back to a client's
+ (i.e. web browser's) request with a 404 response, i.e. resource not found.
+ Your server, however, needs to be able to capture the client's request in
+ full and output it on the command line.
+ *
+*/
+import java.net.*;
+import java.io.*;
+//Class Initializes a ProxyThread
+public class ProxyServer {
+    //Main Class
+    public static void main(String[] args) throws IOException {
+
+
+        //create variable that initializes the port number to 80
+        final int portNumber = 80;
+        //Create a Proxy Server while a Client is Trying to Connect to Port 80
+        try (ServerSocket serverSocket =
+                     new ServerSocket(portNumber)) {
+
+            while(true){
+                //Accept Socket and Start a new Thread
+                new ProxyThread(serverSocket.accept()).start();
+            }
+        }//if client connection is not established then send error message to client
+        catch (IOException e) {
+            System.err.println("Couldn't listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+}
+
---
+++ ProxyThread.java
@@ -0,0 +1,59 @@
+/**
+ * Created by Jorge and Grant Swenson  on 4/6/2015.
+ *
+ *
+ *
+ * Creates a Thread to be used in Multi-Threaded Proxy Server
+ */
+//import Java Classes to  be used in Server
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.net.*;
+import java.io.*;
+
+public class ProxyThread extends Thread{
+    //Initialize a Socket
+    private Socket socket = null;
+    //Class Constructor that Will Take in a Socket for reading and writing
+    public ProxyThread(Socket socket){
+        super("ProxyThread");
+        //Initialize Socket
+        this.socket = socket;
+    }
+    //Initiates Thread to be started
+    public void run() {
+        try (
+                //PrintWriter
+                PrintWriter out = new PrintWriter(socket.getOutputStream());
+                //Open Socket's inputstream to a BufferedReader
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        ) {
+            //create a string variable to input clients sent request
+            String clientInput =null;
+            //While Client is still sending requests, print Information to Command Line
+            while((clientInput = in.readLine()) != null){
+                clientInput = in.readLine();
+                System.out.println(" Received from Client: " + clientInput);
+
+
+            }
+            out.println("404 Error: Page not Found");
+            out.flush();
+            out.close();
+            //After Client stops sending request, close socket
+            socket.close();
+
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    }
+
+
+
+
+
+
