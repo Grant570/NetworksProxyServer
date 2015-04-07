9aeb39536a29d6ddf966ba26b666b56d87b1d892
livelaughconquer <jorgecfelix@gmail.com>
2015-04-06 20:23:50 -0700

    Working!!

--- ProxyThread.java
+++ ProxyThread.java
@@ -23,34 +23,32 @@ public class ProxyThread extends Thread{
     //Initiates Thread to be started
     public void run() {
         try (
-                //PrintWriter
-                PrintWriter out = new PrintWriter(socket.getOutputStream());
                 //Open Socket's inputstream to a BufferedReader
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                //Create a Data OutputStream that will be used to send to a browser
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 
         ) {
             //create a string variable to input clients sent request
             String clientInput =null;
-            //While Client is still sending requests, print Information to Command Line
-            while((clientInput = in.readLine()) != null){
-                clientInput = in.readLine();
+            //While Client request are greater than 5, print Information to Command Line
+            while((clientInput = in.readLine()).length() >= 5){
                 System.out.println(" Received from Client: " + clientInput);
 
-                out.println("404 Error: Page not Found");
-                out.flush();
-                out.close();
-
             }
-
+            //write message in bytes to the Data Output Stream created
+            out.writeBytes("404 Error: Page not Found\r\n");
+            //close data output stream
+            out.close();
             //After Client stops sending request, close socket
             socket.close();
-
-
-
+        
         }catch (IOException e){
             e.printStackTrace();
         }
     }
+
+
     }
 
 
